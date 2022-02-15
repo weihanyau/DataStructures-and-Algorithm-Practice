45c2b71befc926296e9b63282a049a0487eceb88
Wei Han Yau <weihanyau@gmail.com>
2022-02-15 23:22:43 +0800

    More Efficient Solution for addTwoNumbers

--- src/LeetCode/0002AddTwoNumbers.java
+++ src/LeetCode/0002AddTwoNumbers.java
@@ -3,6 +3,35 @@ package LeetCode;
 import java.math.BigInteger;
 
 class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        boolean carry = false;
+        ListNode result = l1;
+        while(l1 != null){
+            if(l2 != null){
+                l1.val += l2.val;
+            }
+            if(carry){
+                l1.val += 1;
+                carry = false;
+            }
+            if(l1.val > 9){
+                l1.val %= 10;
+                carry = true;
+            }
+            if(carry && l1.next == null){
+                l1.next = new ListNode(0);
+            }
+            if(l2 != null){
+                if(l2.next != null && l1.next == null){
+                    l1.next = new ListNode(0);
+                }
+                l2 = l2.next;
+            }
+            l1 = l1.next;
+        }
+        return result;
+    }
+
     public ListNode addTwoNumbersBigInteger(ListNode l1, ListNode l2) {
         BigInteger values = BigInteger.ZERO;
         int l1Size = 0, l2Size = 0;
