1c72b64acdb4e39cf7671f4547c22064e7006a73
Wei Han Yau <weihanyau@gmail.com>
2022-02-19 21:40:09 +0800

    Added two general problems done by me (recursion)

---
+++ src/GeneralProblems/NumWithQuestionMarkAndDivisibleByK.java
@@ -0,0 +1,32 @@
+package GeneralProblems;
+
+public class NumWithQuestionMarkAndDivisibleByK{
+        public static int MaxXORSum(int[] a, int[] b){
+            return MaxXOR(a, b, 0);
+        }
+
+        public static int MaxXOR(int[] a, int[] b, int index){
+            int max = 0, carry = 0;
+            if (index >= a.length){
+                int sum = 0;
+                for(int i = 0; i < a.length; i++){
+                    sum += a[i] ^ b[i];
+                }
+                return sum;
+            } else {
+                for(int i = index; i < a.length; i++){
+                    int temp = b[index];
+                    b[index] = b[i];
+                    b[i] = temp;
+                    carry = MaxXOR(a, b, index + 1);
+                    if(carry > max){
+                        max = carry;
+                    }
+                    temp = b[index];
+                    b[index] = b[i];
+                    b[i] = temp;
+                }
+                return max;
+            }
+        }
+}
