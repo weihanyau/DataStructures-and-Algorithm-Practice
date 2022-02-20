cf1687c87a5bf2e131b5c6846551cfcbc1c3bcbb
Wei Han Yau <weihanyau@gmail.com>
2022-02-20 20:03:50 +0800

    Done middle of a linked list

---
+++ src/LeetCode/0876MiddleOfALinkedList.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class MiddleOfALinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode first = head;
+        ListNode second = head;
+        while(second != null){
+            if(second.next != null){
+                second = second.next.next;
+            } else{
+                break;
+            }
+            first = first.next;
+        }
+        return first;
+    }
+}
