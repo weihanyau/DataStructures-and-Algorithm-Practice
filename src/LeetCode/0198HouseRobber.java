6417e302dd1e26e5eed9036d6511099296638fa2
Wei Han Yau <weihanyau@gmail.com>
2022-02-27 22:58:03 +0800

    Done house robber

---
+++ src/LeetCode/0198HouseRobber.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+class HouseRobber {
+    public int rob(int[] nums) {
+        int total;
+        if(nums.length == 1) return nums[0];
+        nums[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < nums.length; i ++){
+            nums[i] = Math.max(nums[i] + nums[i - 2], nums[i - 1]);
+        }
+        return nums[nums.length - 1];
+    }
+}
