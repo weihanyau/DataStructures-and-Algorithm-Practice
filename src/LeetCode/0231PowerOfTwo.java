d28bd1406d044d373b00ba4cf334be4ad7dcaa4b
Wei Han Yau <weihanyau@gmail.com>
2022-02-28 21:09:26 +0800

    Power of two

---
+++ src/LeetCode/0231PowerOfTwo.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        if((n & n - 1) == 0){
+            return true;
+        }
+        return false;
+    }
+}
