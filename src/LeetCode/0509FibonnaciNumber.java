af5217d2994e11c8f715c01579228dd32fe7e7f4
Wei Han Yau <weihanyau@gmail.com>
2022-03-02 21:37:59 +0800

    Done fibonnaci number and tribonacci number

---
+++ src/LeetCode/0509FibonnaciNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int a = 0, b = 1, c = 0;
+        if(n == 1) return 1;
+        for(int i = 1; i < n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
---
+++ src/LeetCode/1137N-thTribonacciNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n <= 1) return n;
+        if(n == 2) return 1;
+        int a = 0, b = 1, c = 1, ans = 0;
+        for(int i = 2; i < n; i++){
+            ans = a + b + c;
+            a = b;
+            b = c;
+            c = ans;
+        }
+        return ans;
+    }
+}
