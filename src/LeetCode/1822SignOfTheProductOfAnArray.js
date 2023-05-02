b8c5e58c3c65327eac4b85853d7a3325cf8877dd
Wei Han Yau <weihanyau@gmail.com>
2023-05-02 16:51:15 +0800

    Done 1822 leetcode

---
+++ src/LeetCode/1822SignOfTheProductOfAnArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let sign = 1;
+
+    for(let num of nums){
+      if(num === 0){
+        return 0;
+      }
+
+      if(num < 0){
+        sign = -sign;
+      }
+    }
+
+    return sign;
+};
