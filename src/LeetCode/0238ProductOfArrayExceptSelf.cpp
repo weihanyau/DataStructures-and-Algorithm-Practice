04324c4d9e64cbf94e4ef1fdd1e3c59cbf2b9215
weihanyau <weihanyau@gmail.com>
2026-04-17 00:14:13 +0800

    optimize space complexity of 0238

--- src/LeetCode/0238ProductOfArrayExceptSelf.cpp
+++ src/LeetCode/0238ProductOfArrayExceptSelf.cpp
@@ -5,31 +5,20 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefixMul(nums.size());
-        vector<int> suffixMul(nums.size());
         vector<int> result(nums.size());
 
-        prefixMul[0] = nums[0];
+        result[0] = nums[0];
         for(int i = 1; i < nums.size(); i++){
-            prefixMul[i] = prefixMul[i - 1] * nums[i];
+            result[i] = result[i - 1] * nums[i];
         }
 
-        suffixMul[nums.size() - 1] = nums[nums.size() - 1];
-        for(int i = nums.size() - 2; i >= 0; i--){
-            suffixMul[i] = suffixMul[i + 1] * nums[i];
+        int right = nums[nums.size() - 1];
+        result[nums.size() - 1] = result[nums.size() - 2];
+        for(int i = nums.size() - 2; i > 0; i--){
+            result[i] = right * result[i - 1];
+            right *= nums[i];
         }
-
-        for(int i = 0; i < nums.size(); i++){
-            if(i == 0) {
-                result[i] = suffixMul[i + 1];
-                continue;
-            }
-            if(i == (nums.size() - 1)){
-                result[i] = prefixMul[i - 1];
-                continue;
-            }
-            result[i] = prefixMul[i - 1] * suffixMul[i + 1];
-        }
+        result[0] = right;
 
         return result;
     }
